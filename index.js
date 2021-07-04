189b24f5d144d908fdb4d51bb5fbbf31a5fe1441
YodaForce157 <69751882+YodaForce157@users.noreply.github.com>
2021-07-04 09:59:31 +0100

    make it

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+In fizzbuzz if the number is divisable by 3 it is replaced with fizz and 5 is buzz, and if it is divisible by 15 it is replaced by fizzbuzz!
+*/
+let n;
+for (n = 1; n < 78; n++) {
+    if(n % 15 === 0){
+        console.log(`${n} fizzbuzz`)
+    } else if(n % 5 === 0){
+        console.log(`${n} buzz`)
+    } else if(n % 3 === 0){
+        console.log(`${n} fizz`)
+    } else if(n % 3 > 0, n % 5 > 0, n % 15 > 0){
+        console.log(n)
+    }
+}
